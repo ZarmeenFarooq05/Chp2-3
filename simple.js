3577bd98bd81c4c27c2539b4665aeb6608eef7b4
ZarmeenFarooq05 <zfarooq2005@gmail.com>
2024-02-05 19:03:02 +0500

    first commit

---
+++ simple.js
@@ -0,0 +1,52 @@
+//Assignment
+//Date:05/07/2023;
+// Chapter 2
+
+// Q5 and Q9 = ?
+
+var username = "abc"
+
+var message = "Hello World"
+
+var data = 20 +" "+"years old"
+ 
+var book ="I am trying to learn from the Book 'A smarter way to learn JavaScript'"
+
+var email = "abc"+"@"+".com"
+
+ var one = "Pizza"+"                                                                                           "+"Pizz"+"                                                                              "+"Piz"+"                                                                            "+"Pi"+"                                                                "+"                                                                "+"p"
+
+ var p = "Pizza"
+ var p = "Pizz"
+ var p = "Piz"
+ var p = "Pi"
+ var p = "P"
+
+ var z = ''
+ 
+ alert(z)
+ alert(p)
+ alert(one)
+
+alert(username)
+alert(message)
+alert(data)
+alert(email)
+document.write("I can write HTML content through JS");
+document.write("<br><br>")
+
+// Assignment
+// Date:07/07/2023
+// Chapter 3
+// Q2 = ?
+
+var age = 18+" "+"years old"
+var birth_year = "My birth year is"+" "+2005
+var visitor ="John"
+var producttitle = ""
+
+alert(age)
+document.write(" ")
+document.write(birth_year);
+document.write(" ")
+document.write("John Doe ordered 5 T-shirts on XYZ clothing store");
